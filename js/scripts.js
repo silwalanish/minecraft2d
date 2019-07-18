0f9c93e5f7e6f647df197917f72a219fcd507568
silwalanish <silwalanish@gmail.com>
2019-07-18 20:00:19 +0545

    Display Class implemented

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,18 @@
 
 window.onload = () => {
 
-  function runGame () {
-    let display = new Display(document.clientWidth, document.clientHeight);
-    let gameContainer = document.getElementById("game-container");
-    
-    gameContainer.appendChild(display.init());
-  }
-
-  let scriptFiles = [
+  loadScripts([
     "./js/core/controller.js",
     "./js/core/display.js",
     "./js/core/engine.js",
     "./js/core/game.js"
-  ];
+  ], function(){
 
-  loadScripts(scriptFiles, runGame);
+    let display = new Display(document.documentElement.clientWidth, document.documentElement.clientHeight);
+    let gameContainer = document.getElementById("game-container");
+    
+    gameContainer.appendChild(display.init());
+    display.clearColor("#000");
+  });
 
 };
